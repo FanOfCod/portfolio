faa4405c4bf6039750c228449ee8c0519ec07f79
Marcello Marcello <kiwileloo@gmail.com>
2018-04-22 17:53:19 +0200

    added animation to skills page without text

--- app.js
+++ app.js
@@ -1,69 +1,67 @@
-/*var main = function()
-{	
-		
-	function showPage(topage, frompage) {
-		var pageWidth = frompage.width();
-		topage.css("left", pageWidth);
-		topage.addClass("active-page");
-		topage.add(frompage).animate({
-			"left": "-=" + pageWidth + "px"
-		}, 300).promise().done(function() {
-			frompage.removeClass('active-page');
-		});
-	}
-	var pageone = $("#pageone"),
-		pagetwo = $("#pagetwo");
-	$("a").click(function(e) {
-		e.preventDefault();
-		var frompage = $(this).parent(),
-			topage = frompage.siblings();
-		showPage(topage,frompage);
-	});
-			
-	
-}
-*/
-
-
-
-
 $(document).ready(function () {
-	//your code here
-	
-	function showPage(topage, frompage) {
-		var pageWidth = frompage.width();
-		topage.css("left", pageWidth);
-		topage.addClass("active-page");
-		frompage.css("z-index", "0");
-		topage.css("z-index", "1");
-		topage.animate({
-			"left": /*"-=" /*+ pageWidth + + */"0px"
-		}, 300).promise().done(function() {
-			frompage.removeClass('active-page');
-			currentpage = topage;
-		});
+	var currentPage;
+	var currentPageName;
+
+
+	var element = 1;
+	var isAnimationRunning = false;
+	var isChangingPages = false;
+
+	function animationCircle() {
+		element = element > 3 ? element = 1 : element; 
+		$(".graph-" + element).animate({
+			"opacity": "1" }, 2000).promise().done(function() {
+
+
+				$(".graph-" + element).animate({
+					"opacity": "0" }, 2000); 
+					
+					element = element+1;
+					setTimeout(animationCircle,1000);
+			});
 	};
 
-	var currentpage;
 	$(".menu_item").click(function(e) {
 		e.preventDefault();
-		
-		var frompage;
-		console.log('current page is ' + currentpage);
-		if (currentpage == undefined ) {
-			frompage = $("#homepage");
-		}
-		else {
-			frompage = currentpage;
-		}
-		console.log(frompage);
-		//var frompage = $("#homepage");
-		var page =  $(this).data("page");
-		var topage = $('#'+page);
+		var fromPage = currentPage == undefined ?  $("#homepage") : currentPage;
+		var fromPageName = currentPageName == undefined ? "homepage" : currentPageName;
+		var toPage = $('#'+ $(this).data("page"));
+		// //$(this).off("click");
 
-		showPage(topage,frompage);
+		// $(".menu_item").removeClass("active_menu_item");
+		// $(this).addClass("active_menu_item");
+
+		 var toPageName = $(this).data("page");
+		 if (toPageName!== fromPageName && isChangingPages === false) {
+			console.log(fromPageName + ' to ' + toPageName);
+			$(".menu_item").removeClass("active_menu_item");
+			$(this).addClass("active_menu_item");
+			showPage(toPage, fromPage, toPageName);
+
+		};
 	});
-			
-	
-});
 
+	function showPage(toPage, fromPage, toPageName) {
+		isChangingPages=true;
+		var pageWidth = fromPage.width();
+		toPage.css("left", pageWidth);
+		toPage.addClass("active-page");
+		fromPage.css("z-index", "0");
+		toPage.css("z-index", "1");
+		toPage.animate({
+			"left": "0px"
+		}, 300).promise().done(function() {
+			fromPage.removeClass('active-page');
+			currentPage = toPage;
+			currentPageName= toPageName;
+
+			isChangingPages=false;
+
+			if (currentPageName==='skills' && isAnimationRunning === false)
+			{
+				isAnimationRunning = true;
+				setTimeout(animationCircle, 3000);
+			}
+		});
+	};
+});

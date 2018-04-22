ccb85831f765c0425a3a29fbf621a85597eeef31
Marcello Marcello <kiwileloo@gmail.com>
2018-04-22 22:01:46 +0200

    added roboto font

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ $(document).ready(function () {
 
 	var span = document.querySelector(".email");
 
-	span.onclick = function() {
-  	document.execCommand("copy");
+	span.onclick = function () {
+		document.execCommand("copy");
 	}
 
-	span.addEventListener("copy", function(event) {
- 	 event.preventDefault();
- 	 if (event.clipboardData) {
-    event.clipboardData.setData("text/plain", 'mw.majch@gmail.com');
- 	 }
+	span.addEventListener("copy", function (event) {
+		event.preventDefault();
+		if (event.clipboardData) {
+			event.clipboardData.setData("text/plain", 'mw.majch@gmail.com');
+		}
 	});
 
 	var currentPage;
@@ -23,36 +23,39 @@ $(document).ready(function () {
 
 	function animationCircle() {
 		$(".animated-graph").animate({
-			"opacity": "1" }, 2000).promise().done(function() {
+			"opacity": "1"
+		}, 2000).promise().done(function () {
 
 
-		element = element > 3 ? element = 1 : element; 
-		$(".graph-" + element).animate({
-			"opacity": "1" }, 2000).promise().done(function() {
+			element = element > 3 ? element = 1 : element;
+			$(".graph-" + element).animate({
+				"opacity": "1"
+			}, 2000).promise().done(function () {
 
 
 				$(".graph-" + element).animate({
-					"opacity": "0" }, 1000); 
-					
-					element = element+1;
-					setTimeout(animationCircle,100);
+					"opacity": "0"
+				}, 1000);
+
+				element = element + 1;
+				setTimeout(animationCircle, 100);
 			});
 
 		});
 	};
 
-	$(".menu_item").click(function(e) {
+	$(".menu_item").click(function (e) {
 		e.preventDefault();
-		var fromPage = currentPage == undefined ?  $("#homepage") : currentPage;
+		var fromPage = currentPage == undefined ? $("#homepage") : currentPage;
 		var fromPageName = currentPageName == undefined ? "homepage" : currentPageName;
-		var toPage = $('#'+ $(this).data("page"));
+		var toPage = $('#' + $(this).data("page"));
 		// //$(this).off("click");
 
 		// $(".menu_item").removeClass("active_menu_item");
 		// $(this).addClass("active_menu_item");
 
-		 var toPageName = $(this).data("page");
-		 if (toPageName!== fromPageName && isChangingPages === false) {
+		var toPageName = $(this).data("page");
+		if (toPageName !== fromPageName && isChangingPages === false) {
 			console.log(fromPageName + ' to ' + toPageName);
 			$(".menu_item").removeClass("active_menu_item");
 			$(this).addClass("active_menu_item");
@@ -62,7 +65,7 @@ $(document).ready(function () {
 	});
 
 	function showPage(toPage, fromPage, toPageName) {
-		isChangingPages=true;
+		isChangingPages = true;
 		var pageWidth = fromPage.width();
 		toPage.css("left", pageWidth);
 		toPage.addClass("active-page");
@@ -70,15 +73,14 @@ $(document).ready(function () {
 		toPage.css("z-index", "1");
 		toPage.animate({
 			"left": "0px"
-		}, 300).promise().done(function() {
+		}, 300).promise().done(function () {
 			fromPage.removeClass('active-page');
 			currentPage = toPage;
-			currentPageName= toPageName;
+			currentPageName = toPageName;
 
-			isChangingPages=false;
+			isChangingPages = false;
 
-			if (currentPageName==='skills' && isAnimationRunning === false)
-			{
+			if (currentPageName === 'skills' && isAnimationRunning === false) {
 				isAnimationRunning = true;
 				animationCircle();
 			}

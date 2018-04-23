7178e66c23d370206b877298e97f6eaff775ff95
Marcello Marcello <kiwileloo@gmail.com>
2018-04-23 14:11:20 +0200

    added work page

--- app.js
+++ app.js
@@ -1,9 +1,80 @@
 $(document).ready(function () {
 
+
+	
+	$("#pics").click(function() {
+
+		$('.pics-container').css("display", "block");
+		$('.slider-nav').fadeIn(300);
+
+	});
+
+	$('.close-button').click(function() {
+		$('.pics-container').css("display", "none");
+
+	});
+	$('.arrow-next').click(function() {
+		
+		
+		var currentSlide = $('.active-slide');
+		var nextSlide = currentSlide.next();
+
+		var currentDot = $('.active-dot');
+		var nextDot = currentDot.next();
+
+		if(nextSlide.length === 0) {
+		  nextSlide = $('.slide').first();
+		nextDot = $('.dot').first(); }
+		    
+		currentSlide.fadeOut(600).removeClass('active-slide');
+		nextSlide.fadeIn(600).addClass('active-slide');
+
+		currentDot.removeClass('active-dot');
+		nextDot.addClass('active-dot');
+		
+	});
+	
+	$('.arrow-prev').click(function() {
+		
+		
+		var currentSlide = $('.active-slide');
+		var prevSlide = currentSlide.prev();
+		
+		var currentDot = $('.active-dot');
+		var prevDot = currentDot.prev();
+		
+		 if(prevSlide.length === 0) {
+			prevSlide = $('.slide').last();
+			prevDot = $('.dot').last();
+			}
+		
+	
+		currentSlide.fadeOut(600).removeClass('active-slide');
+		prevSlide.fadeIn(600).addClass('active-slide');
+		
+		
+		
+		currentDot.removeClass('active-dot');
+		prevDot.addClass('active-dot');
+	
+	});
+
+
+
+
+
+	//email
+
 	var span = document.querySelector(".email");
 
 	span.onclick = function () {
 		document.execCommand("copy");
+		$(".email b").html('copied');
+
+		$('.email').mouseleave( function() {
+
+			$(".email b").html('copy email');
+		});
 	}
 
 	span.addEventListener("copy", function (event) {
